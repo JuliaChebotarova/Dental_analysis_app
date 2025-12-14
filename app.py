0663cd3d6f3cf028a791ffdb062d473e6bc4a888
Chebotarova Julia <lioness.julia.2005@gmail.com>
2025-12-14 06:14:00 +0200

    Add changes for interface 2

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ conf_threshold = st.sidebar.slider(
 )
 
 # Завантаження фото
-uploaded_file = st.file_uploader(type=["jpg", "png", "jpeg"])
+uploaded_file = st.file_uploader('', type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
@@ -61,4 +61,5 @@ if uploaded_file is not None:
             else:
 
                 st.warning("Патологій не знайдено.")
+
 
